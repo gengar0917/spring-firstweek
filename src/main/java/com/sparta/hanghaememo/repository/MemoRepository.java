ab712508e8893baffc3320d8d515581456378ad4
kes <kmskes1125@gmail.com>
2023-04-19 13:32:58 +0900

    MemoRepository.java 커밋
    MemoRepository.java 커밋함.
    주석을 이용해 설명 추가함.

--- src/main/java/com/sparta/hanghaememo/repository/MemoRepository.java
+++ src/main/java/com/sparta/hanghaememo/repository/MemoRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    //메모를 수정시간 기준으로 내림차순으로 정렬하는 메서드를 만듦
     List<Memo> findAllByOrderByModifiedAtDesc();
 }
