6c56bfc7664a1f887b3bf2b2fdefc78e5cd9e99b
kes <kmskes1125@gmail.com>
2023-04-19 13:35:03 +0900

    MessageService.java 커밋
    MessageService.java 커밋함.
    주석을 이용해 설명 추가함.

--- src/main/java/com/sparta/hanghaememo/service/MessageService.java
+++ src/main/java/com/sparta/hanghaememo/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.sparta.hanghaememo.service;
 
 public class MessageService {
+    //MemoService.deleteMemo() 동작 중 password의 일치 여부에 따라 메세지를 출력하는 메서드
     public static String deleteMessage(boolean isSuccess) {
         if(isSuccess == true){
             return "성공적으로 삭제하였습니다.";
@@ -9,6 +10,7 @@ public class MessageService {
         }
     }
 
+    //MemoService.updateMemo() 동작 중 password가 다를 경우 에러메세지를 출력하게 하는 메서드
     public static String updateMessage(boolean isEquals) {
         String fail = "";
         if(isEquals == false){
@@ -17,6 +19,7 @@ public class MessageService {
         return fail;
     }
 
+    //MemoService.getMemo() 동작 중 id가 다를 경우 에러메세지를 출력하게 하는 메서드
     public static String getMemo() {
         String isNotId = "해당 id의 메모를 찾을 수 없습니다.";
         return isNotId;
